f51186080bb6d7b06d2dfd32e4d8e8ee9d5cd8b4
Santiago Simarro <ssimarro@externos.bankia.com>
2019-04-11 16:04:24 +0200

    primera subida

---
+++ src/main/java/com/santi/demo/PaginaWebApplication.java
@@ -0,0 +1,13 @@
+package com.santi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaWebApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/static/js/index.es6
@@ -0,0 +1,8 @@
+let btnEntrar = document.querySelector('#btnLogin');
+let form = document.querySelector('#form');
+let user = document.querySelector('#inputUser').value;
+let pass = document.querySelector('#inputPass').value;
+
+btnEntrar.onclick = () => {
+    console('form',pass);
+}
